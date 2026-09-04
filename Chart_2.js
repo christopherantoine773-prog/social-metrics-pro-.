b913183bed45499886d3dfaedca432db3d7e6b02
Christopher Antoine <christopherantoine773@gmail.com>
2026-09-04 12:58:30 -0400

    Delete Chart_2.js

--- Chart_2.js
+++
@@ -1,9 +0,0 @@
-# Créer un dossier et y copier les fichiers
-mkdir social-dashboard && cp -r * social-dashboard/
-
-# Serveur local (pour tester)
-python -m http.server
-
-# Ou déployer sur Netlify/Vercel/GitHub Pages :
-git init && git add . && git commit -m "Initial commit"
-gh repo create social-analytics-dashboard && git push origin main
